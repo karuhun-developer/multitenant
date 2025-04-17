72a9fe841074079aa8b370d22c4878026a515b28
Bayu Rifki Alghifari <bayurifkialgh@gmail.com>
2025-04-17 12:27:39 +0700

    Update ShareSetting.php

--- app/Http/Middleware/ShareSetting.php
+++ app/Http/Middleware/ShareSetting.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use App\Models\Setting;
+use App\Models\Tenant;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
@@ -17,6 +18,26 @@ class ShareSetting
      */
     public function handle(Request $request, Closure $next): Response
     {
+        // If not login
+        if (!auth()->check()) {
+            // Check subdomain
+            $host = $request->getHost();
+            $subdomain = explode('.', $host)[0];
+
+            // Not in the main domain
+            if($host !== config('app.main_domain')) {
+                // Check tenant
+                $tenant = Tenant::where('subdomain', $subdomain)->first();
+
+                // Tenant exists
+                if($tenant) {
+                    $settings = Setting::where('tenant_id', $tenant->id)->first();
+                    View::share('settings', $settings);
+                    return $next($request);
+                }
+            }
+        }
+
         // Pas setting
         try {
             $settings = Setting::first();
